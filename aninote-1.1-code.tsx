3b5d5e70999d44726fc42c92effb3ccbf837f9d4
John Jones <110754942+avantjohn@users.noreply.github.com>
2022-08-09 22:48:28 -0700

    created attribute block dynamic add remove and inputs

--- aninote-1.1-code.tsx
+++ aninote-1.1-code.tsx
@@ -18,19 +18,27 @@ const {
 
 function Widget() {
   const [elementList, setElementList] = useSyncedState("", [
-    { elementName: "", elementValue: "" },
+    {
+      elementName: "",
+      elementValue: "",
+      attribute: [{ from: "", to: "", propertyName: "" }],
+    },
   ]);
 
   const handleAddElementBlock = (index: number) => {
     const tempElementList = [...elementList];
-    tempElementList.splice(index + 1, 0, { elementName: "", elementValue: "" });
+    tempElementList.splice(index + 1, 0, {
+      elementName: "",
+      elementValue: "",
+      attribute: [{ from: "", to: "", propertyName: "" }],
+    });
     setElementList(tempElementList);
   };
 
   const handleRemoveElementBlock = (index: number) => {
-    const list = [...elementList];
-    list.splice(index, 1);
-    setElementList(list);
+    const tempElementList = [...elementList];
+    tempElementList.splice(index, 1);
+    setElementList(tempElementList);
   };
 
   const handleElementInput = (index: number, onTextEditEnd: any) => {
@@ -38,6 +46,16 @@ function Widget() {
     const tempElementList = [...elementList];
     tempElementList[index].elementValue = onTextEditEnd.characters;
     setElementList(tempElementList);
+  };
+
+  const addNewProperty = (index: number, attrIndex: number) => {
+    const tempelementList = [...elementList];
+    tempelementList[index].attribute.splice(attrIndex, 0, {
+      from: "",
+      to: "",
+      propertyName: "",
+    });
+    setElementList(tempelementList);
   };
 
   return (
@@ -115,7 +133,7 @@ function Widget() {
           >
             <AutoLayout
               name="element-title-block"
-              fill="#1D1D1D"
+              fill="#232323"
               cornerRadius={4}
               overflow="visible"
               width="fill-parent"
@@ -164,7 +182,6 @@ function Widget() {
                 lineHeight={20}
                 width={"fill-parent"}
                 inputFrameProps={{
-                  fill: "#1D1D1D",
                   cornerRadius: 4,
                   padding: {
                     vertical: 2,
@@ -180,7 +197,6 @@ function Widget() {
                     opacity: 1,
                   }}
                   opacity={0.5}
-                  fill="#1D1D1D"
                   cornerRadius={4}
                   overflow="visible"
                   spacing={8}
@@ -207,18 +223,238 @@ function Widget() {
                 </AutoLayout>
               )}
             </AutoLayout>
-            <AutoLayout
-              name="attribute-block"
-              fill="#2B2B2B"
-              stroke="#000"
-              cornerRadius={4}
-              overflow="visible"
-              direction="vertical"
-              spacing={4}
-              width="fill-parent"
-              height={40}
-              strokeDashPattern={[4, 4]}
-            />
+            {elementBlock.attribute.map((attr, attrIndex) => {
+              return (
+                <AutoLayout
+                  name="property-block"
+                  overflow="visible"
+                  direction="vertical"
+                  spacing={4}
+                  width="fill-parent"
+                >
+                  <AutoLayout
+                    name="property-add-handler"
+                    opacity={0.5}
+                    x={-44}
+                    y={4}
+                    positioning="absolute"
+                    overflow="visible"
+                    horizontalAlignItems="center"
+                    verticalAlignItems="center"
+                    onClick={() => {
+                      addNewProperty(index, attrIndex);
+                    }}
+                  >
+                    <Frame
+                      name="icon-add"
+                      stroke="#AAA"
+                      cornerRadius={14}
+                      width={16}
+                      height={16}
+                    >
+                      <SVG
+                        name="icon-plus"
+                        x={4}
+                        y={4}
+                        height={8}
+                        width={8}
+                        src="<svg width='8' height='8' viewBox='0 0 8 8' fill='none' xmlns='http://www.w3.org/2000/svg'>
+<path fill-rule='evenodd' clip-rule='evenodd' d='M3 7C3 7.55228 3.44772 8 4 8C4.55228 8 5 7.55228 5 7V5H7C7.55228 5 8 4.55228 8 4C8 3.44772 7.55228 3 7 3H5V1C5 0.447715 4.55228 0 4 0C3.44772 0 3 0.447715 3 1V3H1C0.447715 3 0 3.44772 0 4C0 4.55228 0.447715 5 1 5H3V7Z' fill='#AAAAAA'/>
+</svg>
+"
+                      />
+                    </Frame>
+                  </AutoLayout>
+                  <AutoLayout
+                    name="property-title-block"
+                    fill="#232323"
+                    cornerRadius={6}
+                    overflow="visible"
+                    width="fill-parent"
+                  >
+                    <Input
+                      name="property-input"
+                      value={attr.propertyName}
+                      onTextEditEnd={(e) => {
+                        e.characters;
+                        const tempElementList = [...elementList];
+                        tempElementList[index].attribute[
+                          attrIndex
+                        ].propertyName = e.characters;
+                        setElementList(tempElementList);
+                      }}
+                      placeholder="property-name"
+                      fill="#EEE"
+                      width={"fill-parent"}
+                      fontSize={12}
+                      lineHeight={20}
+                      fontWeight={500}
+                      inputFrameProps={{
+                        padding: { vertical: 2, horizontal: 8 },
+                        fill: "#232323",
+                        cornerRadius: 8,
+                      }}
+                      inputBehavior="wrap"
+                    />
+                    <AutoLayout
+                      name="property-remove-handler"
+                      cornerRadius={8}
+                      overflow="visible"
+                      spacing={8}
+                      padding={4}
+                    >
+                      <Frame
+                        name="icon-remove"
+                        opacity={0.5}
+                        stroke="#AAA"
+                        cornerRadius={14}
+                        width={16}
+                        height={16}
+                      >
+                        <Rectangle
+                          name="icon-minus"
+                          x={4}
+                          y={9}
+                          fill="#AAA"
+                          cornerRadius={4}
+                          rotation={90}
+                          width={2}
+                          height={8}
+                        />
+                      </Frame>
+                    </AutoLayout>
+                  </AutoLayout>
+                  <AutoLayout
+                    name="property-value-block"
+                    overflow="visible"
+                    direction="vertical"
+                    spacing={4}
+                    padding={{
+                      top: 0,
+                      right: 0,
+                      bottom: 0,
+                      left: 18,
+                    }}
+                    width="fill-parent"
+                  >
+                    <AutoLayout
+                      name="from-block"
+                      overflow="visible"
+                      spacing={4}
+                      width="fill-parent"
+                      verticalAlignItems="center"
+                    >
+                      <AutoLayout
+                        name="from-label"
+                        overflow="visible"
+                        spacing={8}
+                      >
+                        <Text
+                          name="from:"
+                          fill="#AAA"
+                          lineHeight={20}
+                          fontFamily="Inter"
+                          fontSize={12}
+                          italic={true}
+                          fontWeight={500}
+                        >
+                          from:
+                        </Text>
+                      </AutoLayout>
+                      <AutoLayout
+                        name="from-input"
+                        fill="#1D1D1D99"
+                        cornerRadius={6}
+                        overflow="visible"
+                        spacing={8}
+                        padding={{
+                          vertical: 2,
+                          horizontal: 8,
+                        }}
+                        width="fill-parent"
+                        verticalAlignItems="center"
+                      >
+                        <Input
+                          name="from-input-value"
+                          value={attr.from}
+                          onTextEditEnd={(e) => {
+                            e.characters;
+                            const tempElementList = [...elementList];
+                            tempElementList[index].attribute[attrIndex].from =
+                              e.characters;
+                            setElementList(tempElementList);
+                          }}
+                          placeholder="0"
+                          fill="#EEE"
+                          width={"fill-parent"}
+                          fontSize={12}
+                          lineHeight={20}
+                          fontWeight={500}
+                          inputBehavior="wrap"
+                        />
+                      </AutoLayout>
+                    </AutoLayout>
+                    <AutoLayout
+                      name="to-block"
+                      overflow="visible"
+                      spacing={4}
+                      width="fill-parent"
+                      verticalAlignItems="center"
+                    >
+                      <AutoLayout
+                        name="to-label"
+                        overflow="visible"
+                        spacing={8}
+                      >
+                        <Text
+                          name="to:"
+                          fill="#AAA"
+                          lineHeight={20}
+                          fontFamily="Inter"
+                          fontSize={12}
+                          italic={true}
+                          fontWeight={500}
+                        >
+                          to:
+                        </Text>
+                      </AutoLayout>
+                      <AutoLayout
+                        name="to-input"
+                        fill="#1D1D1D99"
+                        cornerRadius={6}
+                        overflow="visible"
+                        spacing={8}
+                        padding={{
+                          vertical: 2,
+                          horizontal: 8,
+                        }}
+                        width="fill-parent"
+                        verticalAlignItems="center"
+                      >
+                        <Input
+                          name="to-input-value"
+                          value={attr.to}
+                          onTextEditEnd={(e) => {
+                            e.characters;
+                            const tempElementList = [...elementList];
+                            tempElementList[index].attribute[attrIndex].to =
+                              e.characters;
+                            setElementList(tempElementList);
+                          }}
+                          placeholder="100"
+                          fill="#EEE"
+                          width={"fill-parent"}
+                          fontSize={12}
+                          lineHeight={20}
+                          fontWeight={500}
+                          inputBehavior="wrap"
+                        />
+                      </AutoLayout>
+                    </AutoLayout>
+                  </AutoLayout>
+                </AutoLayout>
+              );
+            })}
           </AutoLayout>
         </AutoLayout>
       ))}
